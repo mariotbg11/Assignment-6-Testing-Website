8ddbba244bc3da462678c76a94675c22c8dcfb00
mariotbg11 <christofell.m@gmail.com>
2024-10-04 23:29:00 +0700

    test: testing myrecipe-website with jest

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,57 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import App from "./App";
 
-test('renders App component with NavBar, image banner, and Footer', async () => {
+test("renders App component with NavBar, image banner, and Footer", async () => {
   render(<App />);
 
-  // image banner is rendered 
-  const banner = screen.getByTestId('image-banner')
+  // image banner is rendered
+  const banner = screen.getByTestId("image-banner");
   expect(banner).toBeInTheDocument();
-  expect(banner).toHaveProperty('src', 'change-to-link-url')
+
+  expect(banner).toHaveProperty(
+    "src",
+    "https://www.instacart.com/company/wp-content/uploads/2022/11/cooking-statistics-hero.jpg"
+  ); // test image banner src property
+
+  expect(banner).toHaveProperty("alt", "banner"); // test image banner alt property
+
+  ////////////////////////////////////////////////
+  // Navbar h3 is rendered
+  const navTitle = screen.getByTestId("my-recipe");
+  expect(navTitle).toBeInTheDocument();
+
+  expect(navTitle.innerHTML).toBe("My Recipe"); // test h3 element on Navbar with text "My Recipe"
+
+  // Form is rendered
+  const form = screen.getByTestId("form-search");
+  expect(form).toBeInTheDocument();
+
+  //////////////////////////////////////////////
+  // Footer text is rendered
+  const footerText = screen.getByTestId("footer-text");
+  expect(footerText).toBeInTheDocument();
+
+  expect(footerText.innerHTML).toBe(
+    "© 2024 Company, Inc. All rights reserved."
+  ); // test p element with text "© 2024 Company, Inc. All rights reserved."
+
+  // Facebook icon is rendered
+  const facebook = screen.getByTestId("link-facebook");
+  expect(facebook).toBeInTheDocument();
+
+  expect(facebook).toHaveAttribute("href", "https://facebook.com"); // test href anchor facebook
+
+  // X icon is rendered
+  const x = screen.getByTestId("link-x");
+  expect(x).toBeInTheDocument();
+
+  expect(x).toHaveAttribute("href", "https://x.com"); // test href anchor X
+
+  // Instagram icon is rendered
+  const instagram = screen.getByTestId("link-instagram");
+  expect(instagram).toBeInTheDocument();
+
+  expect(instagram).toHaveAttribute("href", "https://instagram.com/"); // test href anchor instagram
 });
--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import Card from './Card';
+import React from "react";
+import { screen, render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Card from "./Card";
 
 const el = {
-  image: 'https://via.placeholder.com/150',
-  name: 'Test Recipe',
+  image: "https://via.placeholder.com/150",
+  name: "Test Recipe",
   rating: 4.5,
-  tags: ['Tag1', 'Tag2', 'Tag3'],
-  id: 1
+  tags: ["Tag1", "Tag2", "Tag3"],
+  id: 1,
 };
 
-test('renders the component Card & given props', () => {
+test("renders the component Card & given props", () => {
   render(<Card el={el} />);
 
+  // img is rendered
+  const img = screen.getByTestId(`img-recipe-${el.id}`);
+  expect(img).toBeInTheDocument();
+
+  expect(img).toHaveAttribute("alt", el.name); // test img attr alt with el.name
+
+  // title is rendered
+  const title = screen.getByTestId(`title-recipe-${el.id}`);
+  expect(title).toBeInTheDocument();
+
+  expect(title.innerHTML).toBe(el.name); // test title h5 element with text "el.name"
+
+  // rating is rendered
+  const rating = screen.getByTestId(`rating-recipe-${el.id}`);
+  expect(rating).toBeInTheDocument();
+
+  expect(rating.innerHTML).toBe(el.rating.toString()); // test rating p element with text "el.rating"
+
+  // rating is rendered
+  const link = screen.getByTestId(`link-recipe-${el.id}`);
+  expect(link).toBeInTheDocument();
+
+  expect(link).toHaveAttribute(
+    "href",
+    `https://dummyjson.com/recipes/${el.id}`
+  ); // test link anchor element with href "https://dummyjson.com/recipes/${el.id}"
 });
